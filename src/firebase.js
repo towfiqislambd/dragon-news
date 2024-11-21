e97765c18bedca41a293a23d5646bdc53ee7b29e
towfiqislambd <towfiqislambd@gmail.com>
2024-11-22 00:31:02 +0600

    last commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4PaLxCYd9GKzfs0SBd5LNP_Nk_YF9qnQ",
-  authDomain: "simple-firebase-aa838.firebaseapp.com",
-  projectId: "simple-firebase-aa838",
-  storageBucket: "simple-firebase-aa838.firebasestorage.app",
-  messagingSenderId: "778069379994",
-  appId: "1:778069379994:web:ac9b9e5a8f15113e5f8823"
-};
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
+}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
